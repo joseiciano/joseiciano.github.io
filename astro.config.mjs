4c094cea6a187499c1548c53df936fe1f9c94d9f
joseiciano <icianojn@gmail.com>
2024-07-02 20:56:22 -0400

    Added blog section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import metaTags from "astro-meta-tags";
+import solidJs from "@astrojs/solid-js";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
   site: "https://joseiciano.github.io/",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), solidJs(), metaTags(), robotsTxt()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,7 +21,8 @@ module.exports = {
       // "cupcake",
       // "light",
     ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
+    // darkTheme: "dark", // name of one of the included themes for dark mode,
+    darkTheme: "cupcake",
     logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
   },
 };
